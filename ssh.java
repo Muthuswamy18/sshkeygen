71922acb68f62c52ab7673f5927111ae24a07cf5
Muthuswamy18 <85876345+Muthuswamy18@users.noreply.github.com>
2022-02-09 15:59:16 +0530

    added comments to the file

--- ssh.java
+++ ssh.java
@@ -10,7 +10,7 @@
 9     system.out.println("don't even think about it");
 10    system.out.println("look!!");   
 11   }
-12   
+12   #comments are from remote to local
 9     system.out.println("don't even think about it"); 
 10    system.out.println("look!!");
 11   }
