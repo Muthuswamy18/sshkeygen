0c8e7ad8f31706454dfa8f014e7dfc226dc552bd
muthuswamy <muthuswamyseni@gmail.com>
2022-02-09 12:21:38 +0530

    added style in it

--- ssh.java
+++ ssh.java
@@ -1,12 +1,12 @@
-public class main{
-      
-	public static void main(String []args){
-     
-      <1-- java style used -->
-     system.out.println("sshkey");
-     system.out.println("hello world");
-     system.out.println("hi");
-}
-
-}
-
+1 public class main{
+2
+3      public static void main(String []args){
+4
+5     <1-- java style used -->
+6    system.out.println("sshkey");
+7     system.out.println("hello world"); 
+8     system.out.println("hi");
+9    }
+10
+11   }
+12
