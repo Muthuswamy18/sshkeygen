709c7927d18654b37d805f65b5d54c70476fc7db
Muthuswamy18 <85876345+Muthuswamy18@users.noreply.github.com>
2022-02-09 12:11:48 +0530

    added comments

--- ssh.java
+++ ssh.java
@@ -1,7 +1,8 @@
 public class main{
       
 	public static void main(String []args){
-      
+     
+      <1-- java style used -->
      system.out.println("sshkey");
      system.out.println("hello world");
      system.out.println("hi");
